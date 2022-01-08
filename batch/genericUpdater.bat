97f6fa54982238a0ae106f1c045bfc49c9e6c551
XaaRii <54722170+XaaRii@users.noreply.github.com>
2022-01-09 00:06:35 +0100

    Add files via upload

---
+++ batch/genericUpdater.bat
@@ -0,0 +1,45 @@
+@echo off
+:: 3, 4, 26, 29
+set version=5
+set serverfile=generic.bat
+IF /i "%~dp0"=="%localappdata%\PaweleConf\" (
+  if "%1" == "update" (
+    if defined updn (
+      curl -L https://raw.githubusercontent.com/XaaRii/XaaRis_scripts/main/batch/%serverfile% -o "%localappdata%/PaweleConf/temp" 2> NUL
+      FOR /F "tokens=*" %%g IN ('powershell Get-Content %localappdata%\\PaweleConf\\temp -Head 1') do (
+        if "%%g" EQU "404: Not Found" (
+          echo "404: Not Found" > %localappdata%\\PaweleConf\\lasterror
+          del %localappdata%\\PaweleConf\\temp
+          exit /B 1
+        )
+      )
+      move /y "%localappdata%\\PaweleConf\\temp" "%updn%" >nul
+      if exist %localappdata%\\PaweleConf\\lasterror del %localappdata%\\PaweleConf\\lasterror
+      cls
+      %updn%
+      goto :EOF
+    ) else echo Something's broken. Cannot find variable. Exiting... & exit /B 1
+  ) else exit 0
+)
+if exist %localappdata%\\PaweleConf\\%~nx0 del %localappdata%\\PaweleConf\\%~nx0
+powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/XaaRii/XaaRis_scripts/main/versions.ini -OutFile "%localappdata%/PaweleConf/versions.ini"
+  for /f "delims=" %%x in (%localappdata%/PaweleConf/versions.ini) do %%x 2>NUL
+  IF %version% NEQ %versionGeneric% call :update
+  goto :main
+:update
+echo Current version: %version%               Available version: %versionGeneric%
+if exist %localappdata%/PaweleConf/lasterror (
+  echo. & echo Warning! Last update failed with following error code: & powershell Get-Content %localappdata%\\PaweleConf\\lasterror -Head 1 & echo.
+)
+CHOICE /C yn /N /M "Do you want to update? (press Y or N)"
+if "%errorlevel%"=="1" echo Updating...
+if "%errorlevel%"=="2" goto :EOF
+SETLOCAL
+IF /i NOT "%~dp0"=="%localappdata%/PaweleConf/" (
+  set updn=%~dpnx0
+  COPY /y "%~dpnx0" "%localappdata%/PaweleConf/%~nx0" >nul
+  %localappdata%/PaweleConf/%~nx0 update
+  goto :EOF
+)
+:main
+REM ------------------ PROGRAM HERE ------------------------
