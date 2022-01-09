e82ae85c1f55971e4ed1262dacea65c9c2adaf64
XaaRii <54722170+XaaRii@users.noreply.github.com>
2022-01-09 19:26:11 +0100

    Update powercord-updater.bat

--- batch/powercord-updater.bat
+++ batch/powercord-updater.bat
@@ -1,9 +1,10 @@
 @echo off
-set version=7.3
+set version=7.4
 set serverfile=powercord-updater.bat
 IF /i "%~dp0"=="%localappdata%\PaweleConf\" (
   if "%1" == "update" (
     if defined updn (
+      title Downloading update...
       curl -L https://raw.githubusercontent.com/XaaRii/XaaRis_scripts/main/batch/%serverfile% -o "%localappdata%/PaweleConf/temp" 2> NUL
       FOR /F "tokens=*" %%g IN ('@powershell Get-Content %localappdata%\\PaweleConf\\temp -Head 1') do (
         if "%%g" EQU "404: Not Found" (
@@ -20,12 +21,14 @@ IF /i "%~dp0"=="%localappdata%\PaweleConf\" (
     ) else echo Something's broken. Cannot find variable. Exiting... & exit /B 1
   ) else exit 0
 )
+title Update check
 echo Checking for updates...
 if exist %localappdata%\\PaweleConf\\"%~nx0" del %localappdata%\\PaweleConf\\"%~nx0"
 @powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/XaaRii/XaaRis_scripts/main/versions.ini -OutFile "%localappdata%/PaweleConf/versions.ini"
   for /f "delims=" %%x in (%localappdata%/PaweleConf/versions.ini) do %%x 2>NUL
   cls
   IF %version% NEQ %versionPowercordUpdater% call :update
+  title  
   goto :main
 :update
 echo Current version: %version%               Available version: %versionPowercordUpdater%
