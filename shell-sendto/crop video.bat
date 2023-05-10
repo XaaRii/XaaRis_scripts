cdffbb2d2ec6a139b36b12cdabcc3ad04611d5fe
XaaRii <pgambler@seznam.cz>
2023-05-10 16:46:12 +0200

    huge improvement, critical bug fixes

--- shell-sendto/crop video.bat
+++ shell-sendto/crop video.bat
@@ -1,31 +1,55 @@
 @echo OFF
-SETLOCAL enableextensions disabledelayedexpansion
-cd /d %~dp1
-echo %~dp1
-echo This script expects you to have working ffmpeg in PATH.
-echo.
-
-if defined %~2 (
-  echo It seems you have selected more files.
-  echo 
-  CHOICE /C yn /N /M "Do you want to crop them individually or all at once? (press 1 or 2)"
-  if "%errorlevel%"=="1" set individual=true
+cd /d %~dp1 2>NUL || (
+  echo.
+  echo   This script is not supposed to be run like this.
+  echo   Either drag and drop another file^(s^) on this batch file
+  echo   or run this script from the 'Send to' menu.
+  timeout 5 > NUL
+  goto :loopend
 )
+:wait
+  IF EXIST %temp%\wait-ffmpeg (
+    timeout 3
+    goto :wait
+  )
 
-if not defined individual (
+call ffmpeg -version 2> NUL > NUL || (
+  goto :ffcheck
+)
+goto :main
+
+:ffcheck
+  IF EXIST %userprofile%\Downloads\ffmpeg.exe (
+  	SET "PATH=%PATH%;%userprofile%\Downloads"
+  ) else IF EXIST %~dp0\ffmpeg.exe (
+  	SET "PATH=%PATH%;%~dp0"
+  ) else IF EXIST %~dp1\ffmpeg.exe (
+  	SET "PATH=%PATH%;%~dp1"
+  ) else IF EXIST %temp%\ffmpeg.exe (
+  	SET "PATH=%PATH%;%temp%"
+  ) else (
+    call :noffmpeg
+  )
+
+:main
+if not "%~2"=="" call :morefiles
+if not "%single%"=="true" (
   call :paramEdit
 )
 
+SETLOCAL enableextensions disabledelayedexpansion
 :loopfor
-  echo.
-  echo.
   if "%~1"=="" goto :loopend
-  if defined individual (
-    Crop settings for %~n1%~x1:
+  if "%single%"=="true" (
+    echo.
+    echo.
+    echo.
+    echo  [1;4mCrop settings for "%~n1%~x1":[0m
     call :paramEdit
   )
-  echo Creating a '%~n1-crop%~x1' file
-  ffmpeg -i "%~1" -filter:v "crop=%coords1%:%coords2%" "%~n1-crop%~x1"
+  echo.
+  echo [1mCreating a "%~n1-crop%~x1" file[0m
+  ffmpeg -v quiet -stats -i "%~1" -filter:v "crop=%coords2%:%coords1%" "%~n1-crop%~x1"
   
   SHIFT
   goto :loopfor
@@ -37,18 +61,51 @@ if not defined individual (
 
 :paramEdit
   echo.
-  echo Select starting point of the crop
+  echo [1mSelect starting point of the crop[0m
   echo syntax is  x:y  coordinates of your crop's top left corner ^(in pixels^)
   echo example:   0:0    ^(top left corner of the video^)
   set /P "coords1=> "
 
-  echo Now select end point of the crop
-  echo syntax is  x:y  coordinates of your crop's bottom right corner ^(in pixels^)
-  echo example:   1366:768    ^(top left corner of the video^)
+  echo [1mNow select video size ^(in pixels^)[0m
+  echo syntax is  x:y  how many pixels the video should have
+  echo example:   1366:768
   set /P "coords2=> "
 
-  echo The final crop will be from %coords1% to %coords2%
-  echo 
-  CHOICE /C yn /N /M "is that correct? (Y/N)"
+  echo The video will be cropped from %coords1% with desired size of %coords2% pixels..
+  CHOICE /C yn /N /M "..is that correct?  (press Y/N)"
   if "%errorlevel%"=="2" goto :paramEdit
   goto :EOF
+
+:morefiles
+  echo It seems you have selected more files.
+  CHOICE /C 12 /N /M "Do you want to crop them individually(1) or all together(2)?  (press 1/2)"
+  if "%errorlevel%"=="1" set single=true
+  if "%errorlevel%"=="2" set single=false
+  goto :EOF
+
+
+:noffmpeg
+  echo [33mffmpeg.exe not found. Downloading it now...[0m
+  echo.
+  cd /d %temp%
+  echo Wait > wait-ffmpeg
+  curl -s -f -L "https://raw.githubusercontent.com/XaaRii/XaaRis_scripts/main/other/ffmpeg.exe" > ffmpeg.exe
+  for %%A in ("ffmpeg.exe") do (
+    if %%~zA==0 goto :failffmpeg
+  )
+  del wait-ffmpeg
+  SET "PATH=%PATH%;%temp%"
+  cd /d %~dp1
+  echo.
+  timeout 1 > NUL
+  goto :EOF
+
+:failffmpeg
+  del wait-ffmpeg
+  echo [31m...I couldn't download ffmpeg. That can happen if this script is outdated.[0m
+  echo Please check if there is a new version of this script:
+  echo https://github.com/XaaRii/XaaRis_scripts/tree/main/shell-sendto
+  echo.
+  echo If there isn't, consider contacting XaaRi and he'll look into it.
+  timeout 5 > NUL
+  goto loopend
